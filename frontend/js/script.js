d608b208c255922d61d3d83fb35f6296e78dc7a4
harigs06 <hgangasai@gmail.com>
2026-02-28 14:51:43 +0530

    home page updated

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,15 @@
 // Just for sample ..
+const text = "Secure File Sharing, Simplified.";
+const typingElement = document.querySelector(".typing");
+
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        typingElement.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 120);
+    }
+}
+
+typeEffect();
