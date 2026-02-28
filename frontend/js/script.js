2ce2e58be6501262e65f01e9ea01412aa56deaef
harigs06 <hgangasai@gmail.com>
2026-02-28 16:10:22 +0530

    Add Contact content

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -13,3 +13,40 @@ function typeEffect() {
 }
 
 typeEffect();
+
+
+const navRight = document.getElementById("nav-right");
+
+function renderNavbar() {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (isLoggedIn === "true") {
+        navRight.innerHTML = `
+            <a href="#">Upload</a>
+            <a href="#features">About</a>
+            <a href="#contact">Contact</a>
+            <img src="/assets/imgs/person.png" class="account" id="logoutBtn">
+        `;
+
+        document.getElementById("logoutBtn").addEventListener("click", () => {
+            localStorage.removeItem("isLoggedIn");
+            renderNavbar();
+        });
+
+    } else {
+        navRight.innerHTML = `
+            <a href="#">Upload</a>
+            <a href="#features">About</a>
+            <a href="#contact">Contact</a>
+            <a href="#" id="loginBtn">Sign In</a>
+            <a href="#">Sign Up</a>
+        `;
+
+        document.getElementById("loginBtn").addEventListener("click", () => {
+            localStorage.setItem("isLoggedIn", "true");
+            renderNavbar();
+        });
+    }
+}
+
+renderNavbar();
